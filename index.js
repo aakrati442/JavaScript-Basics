44be1b7d223bf55f3120538578521f8c9ae0670b
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-08-01 10:45:45 +0530

    commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//print in js
+
+//console.log("Hello WORLD");
+//document.write("this document.write");
+//alert("this is alert");
+
+//js console api
+
+//console.warn("this is warning");
+//console.error("this is error");
+//console.assert((6-2)==4);
+//console.log("hello",4+6);
+
+//js variables
+//variables --containers to store data variables
+
+var num1=34;
+var num2=56;
+console.log(num1*num2);
+//var,let,const-- 
+//var -> accessed within a func
+//const -> cant be reassigned but can be redeclare
+//let -> reassigned but not redeclare 
+
+//data types in js
+
+//string
+var str1="This is str1";
+var str2="This is str2";
+
+//numbers
+var num1=9;
+var num2=8;
+
+//objects
+var marks={
+    ravi:34,
+    shubham:32
+}
+console.log(marks);
+
+//boolean
+var a=true;
+var b=false;
+console.log(a,b);
+
+var und=undefined;
+console.log(und);
